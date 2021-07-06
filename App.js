6b931a0f82e57b6008d4738266174adee48797d1
user_name <raziqali0306@gmail.com>
2021-07-07 04:12:13 +0530

    added states

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,26 +6,56 @@ import {
   View,
   KeyboardAvoidingView,
   TouchableOpacity,
+  Keyboard,
+  ScrollView,
 } from 'react-native';
 import Task from './Components/Task';
 
 const App = () => {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const addTextHandler = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  const removeTask = index => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
       {/* Todo screen */}
       <View style={styles.taskContainer}>
         <Text style={styles.title}>Today's tasks</Text>
-        <View style={styles.body}>
-          <Task text={'Task - 1'} />
-          <Task text={'Task - 2'} />
-        </View>
+        <ScrollView style={styles.body}>
+          {/* here is where all tasks go */}
+          <View>
+            {taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => removeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </ScrollView>
       </View>
 
       {/* Add a task */}
       <KeyboardAvoidingView style={styles.TaskinputContainer}>
-        <TextInput style={styles.input} placeholder={'Write a task...'} />
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task...'}
+          value={task}
+          onChangeText={text => setTask(text)}
+        />
 
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => addTextHandler()}>
           <View style={styles.buttonwrap}>
             <Text style={styles.buttontext}>+</Text>
           </View>
@@ -36,24 +66,27 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  // Todo
   container: {
     flex: 1,
     backgroundColor: '#F9F9F9',
   },
   taskContainer: {
-    marginTop: 94,
+    marginTop: 92,
     marginHorizontal: 20,
   },
   title: {
+    color: '#000000',
     fontFamily: 'Roboto',
     fontSize: 24,
     fontWeight: '700',
   },
   body: {
     marginTop: 30,
-    marginHorizontal: 20,
+    height: '78%',
   },
-  ////////////////////////////////////////////////////////
+
+  // Tasks
 
   TaskinputContainer: {
     marginTop: 15,
